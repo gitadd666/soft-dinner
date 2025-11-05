b3e7bf3b3fc056848e98c0e62bcda5ee660c8d5f
VIDAL FERNANDO VERDUGO ARREDONDO <24030500@uadeo.mx>
2025-11-04 22:04:11 -0700

    actualizacion de base de datos

--- database/scriptMySql.sql
+++ database/scriptMySql.sql
@@ -6,8 +6,8 @@ CREATE TABLE IF NOT EXISTS usuarios (
     id_usuario INT AUTO_INCREMENT PRIMARY KEY,
     nombre VARCHAR(100) NOT NULL,
     correo VARCHAR(100) UNIQUE NOT NULL,
-    contrasena VARCHAR(255) NOT NULL,
-    rol ENUM('admin', 'mesero', 'cocinero', 'cajero') DEFAULT 'mesero'
+    contrasena VARCHAR(255) NOT NULL
+    
 );
 
 -- 2. Mesas del restaurante

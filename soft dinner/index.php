a01b0c9e1ae8d435caf31618ddcfd493cc26cd00
VIDAL FERNANDO VERDUGO ARREDONDO <24030500@uadeo.mx>
2026-06-01 09:35:24 -0700

    Cambio de color Index

--- soft dinner/index.php
+++ soft dinner/index.php
@@ -9,7 +9,7 @@
         @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;600;700&display=swap');
 
         body {
-            background: linear-gradient(135deg, #F0AD69 0%, #d84370 100%);
+            background: linear-gradient(135deg, #000000 0%, #d84370 100%);
             font-family: 'Poppins', sans-serif;
             margin: 0;
             padding: 0;

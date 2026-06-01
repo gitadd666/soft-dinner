e06a4d0587557e3703cf687fa31129d7b73f26bd
VIDAL FERNANDO VERDUGO ARREDONDO <24030500@uadeo.mx>
2026-05-31 22:39:08 -0700

    cambio de color de interfa principales

--- soft dinner/index.php
+++ soft dinner/index.php
@@ -17,7 +17,7 @@ $usuario = "Usuario"
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>Restaurante</title>
 	<style> body {
-        background-color: #F0AD69;
+        background-color: #f069ce;
                  }
     </style>
 </head>
